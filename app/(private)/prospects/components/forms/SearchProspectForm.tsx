39ddc339603375d0c20fef17dc93a16170cdd35c
Assadi <62776480+assadi-dev@users.noreply.github.com>
2026-09-07 19:06:05 +0200

    feat: add 3-second delay before creating search prospect in SearchProspectForm

--- app/(private)/prospects/components/forms/SearchProspectForm.tsx
+++ app/(private)/prospects/components/forms/SearchProspectForm.tsx
@@ -14,6 +14,7 @@ import { useRouter } from "next/navigation";
 import { toast } from "@/lib/toaster";
 import { reportErrorClient } from "@/lib/report-error-client";
 import { dialogMessages } from "../../services/dialog-messages";
+import { waitDelay } from "@/lib/utils";
 
 
 
@@ -31,6 +32,7 @@ export const SearchProspectForm = ({ redirect }: { redirect?: boolean }) => {
 
     const onSubmit = async (data: LeadFinderFormSchemaType) => {
         try {
+            await waitDelay(3000)
             await createSearchProspect(data)
             close();
             if (redirect) {
